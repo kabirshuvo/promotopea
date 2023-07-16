bdcde0d23b42d34677850baacec667e24eb2fea1
kabir shuvo <kabirshuvo19@gmail.com>
2023-07-17 01:30:22 +0600

    Basci Home page done, Need to create some components

--- app/page.jsx
+++ app/page.jsx
@@ -4,7 +4,12 @@ const Home = () => {
     <section className='w-full flex-center flex-col'>
     <h1 className="head_text text-center">
     Discover and Share
+    <br className="max-md:hidden"/>
+    <span className="orange_gradient">AI-Powered Promotions</span>
         </h1> 
+        <p className="desc text-center">
+            Promotopea is an open-source AI prompting tool for modern world to discover, create and share creative prompts
+        </p>
     </section>
   )
 }
