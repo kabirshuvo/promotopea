f7715f09f5a75d7b129f4f9fc1335d9a76f303b0
kabir shuvo <kabirshuvo19@gmail.com>
2023-07-17 15:48:35 +0600

    Working on create-prompt page along with From component

--- app/page.jsx
+++ app/page.jsx
@@ -8,7 +8,7 @@ const Home = () => {
     <br className="max-md:hidden"/>
     <span className="orange_gradient">AI-Powered Promotions</span>
         </h1> 
-        <p className="desc text-center">
+        <p className="description text-center">
             Promotopea is an open-source AI prompting tool for modern world to discover, create and share creative prompts
         </p>
         <Feed/>
